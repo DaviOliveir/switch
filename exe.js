e87445d69bda18c3d617f2c3432f074b29fd2693
Davi José batista Lima de Oliveira <112220168+DaviOliveir@users.noreply.github.com>
2023-04-11 18:42:05 -0300

    Add files via upload

---
+++ exe.js
@@ -0,0 +1,35 @@
+let agora = new Date
+
+let dia = agora.getDay()
+
+console.log(dia)
+
+switch(dia){
+    case 0:
+    console.log('Hoje é domingo ')
+    break
+
+    case 1 :
+    console.log('Hoje é segunda')
+    break
+
+    case 2:
+    console.log('Hoje é terça')
+    break
+
+    case 3:
+    console.log('Hoje é quarta')
+    break
+
+    case 4:
+    console.log('Hoje é quinta')
+    break
+
+    case 5:
+    console.log('Hoje é sexta')
+    break
+
+    case 6:
+    console.log('Hojse é sabado')
+    break
+}
